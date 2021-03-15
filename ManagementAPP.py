ec29c7a551369bfa01a9ac02e84b732a3f3df68e
mperl <mperlitch55@gmail.com>
2021-03-15 16:04:44 -0400

    Added the publishing of topics to server

--- ManagementAPP.py
+++ ManagementAPP.py
@@ -4,9 +4,11 @@ print("Please enter your name")
 name = input()
 print("Name entered:", name)
 
-print("Please et your preferred temperature (Celsius)")
+print("Please set your preferred temperature (Celsius)")
 temp = input()
 print("Temperature entered:", temp)
 
 mqttc = mqtt.Client()
-mqttc.connect(host, port=1883, keepalive=60, bind_address="")
+mqttc.connect("test.mosquitto.org", 1883, 60)
+mqttc.publish("Family Members", payload=name, qos=0, retain=False)
+mqttc.publish("Preferred Temp", payload=temp, qos=0, retain=False)
--- SmartDoorLocker.py
+++ SmartDoorLocker.py
@@ -1,4 +1,14 @@
 import paho.mqtt.client as mqtt
 
+print("Leaving or entering? (1 for entering, 0 for leaving)")
+index = input()
+print("Code entered:", index)
+
+print("Name of person entering or leaving:")
+name = input()
+print("Name entered:", name)
+
 mqttc = mqtt.Client()
-mqttc.connect(host, port=1883, keepalive=60, bind_address="")
+mqttc.connect("test.mosquitto.org", 1883, 60)
+mqttc.publish("Entering/Leaving", payload=index, qos=0, retain=False)
+mqttc.publish("Member entering/leaving", payload=name, qos=0, retain=False)
