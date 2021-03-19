7eb83e4b2d2e8a4e81da26a3d4a944a881f8301b
NK2306 <khoinguyen2306@gmail.com>
2021-03-19 12:48:44 -0400

    Minor fix to broker error handling

--- broker.py
+++ broker.py
@@ -4,7 +4,7 @@ import os
 
 try:    
     subprocess.run(["mosquitto"])
-except:
+except FileNotFoundError:
     print("Mosquitto isn't installed. The script will install it now")
     subprocess.check_call(["sudo", "apt-add-repository", "-y", "ppa:mosquitto-dev/mosquitto-ppa"])
     subprocess.check_call(["sudo", "apt", "install", "-y", "mosquitto"])
